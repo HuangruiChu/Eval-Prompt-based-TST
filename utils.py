9db3461b2245e74102f767c959462f6f7021bd70
David Peng <david.peng@yale.edu>
2023-04-08 11:46:45 -0400

    specify gen_prompt input

--- utils.py
+++ utils.py
@@ -3,13 +3,17 @@
 def gen_prompt(input_sentence,label,prompt_style="zero_shoot"):
   '''
   generate the prompt for the specific sentence
-
+  
+  label should be in ["neg", "pos"]
   
   '''
-  to_style = "negative"
-  
-  if label == "neg":
+
+  if label == "pos":
+    to_style = "negative"
+  elif label == "neg":
       to_style = "positive"
+  else:
+    raise Exception(f"Label \"{label}\" is not either \"neg\" or \"pos\".")
 
   if prompt_style == "zero_shoot":
     answer = '''Here is some text: \"{}\". Here is a rewrite of the text, which is more {}: \"'''.format(input_sentence,to_style)
