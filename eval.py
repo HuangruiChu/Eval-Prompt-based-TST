edb37bede27e2fd334f1431a760c8f7ca8587ddd
David Peng <david.peng@yale.edu>
2023-04-26 00:06:16 -0400

    add score on sentiment classifer

--- eval.py
+++ eval.py
@@ -2,11 +2,13 @@ import pandas as pd
 import numpy as np
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
 
-
-def classify(pred_texts):
+def classify_sentiment(pred_texts):
     """
     Classify list of texts using a finetuned RoBERTa-Large (SiEBERT)
+
+    {0: "NEGATIVE", 1: "POSITIVE"}
     """
+
     model_name = "siebert/sentiment-roberta-large-english"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
@@ -21,7 +23,8 @@ def classify(pred_texts):
         
         def __getitem__(self, idx):
             return {k: v[idx] for k, v in self.tokenized_texts.items()}
-    
+
+
     tokenized_texts = tokenizer(pred_texts,truncation=True,padding=True)
     pred_dataset = SimpleDataset(tokenized_texts)
 
@@ -33,6 +36,42 @@ def classify(pred_texts):
     
     return preds, labels, scores
 
+def score_sentiment(generated, target_class="NEGATIVE", output_file=None):
+    """Give the fraction and percentage of successful sentiment transfers
+    
+    Assumes all of the generated sentences are trying to be the target_class
+    
+    {0: "NEGATIVE", 1: "POSITIVE"}
+    """
+
+    assert target_class in ["NEGATIVE", "POSITIVE"]
+
+    preds, _, _ = classify_sentiment(generated)
+    
+    total = len(preds)
+    score = sum(preds)
+    if target_class == "NEGATIVE":
+        score = total - sum(preds)
+
+    if output_file:
+        if target_class == "NEGATIVE":
+            expected_scores = total * [0]
+        else:
+            expected_scores = total * [1]
+        individual_scores = [classified == expected for (classified, expected) in zip(preds, expected_scores)]
+        print(f"Saving in new file {output_file}")
+        pd.DataFrame(individual_scores).to_csv(output_file, index=False, header=None)
+
+    return f"{(score)/total:.2f} ({score}/{total})"
+
+
 if __name__ == "__main__":
-    pred_texts = ["ever since joes has changed hands it 's just gotten worse and worse ."]
-    print(classify(pred_texts))
+    #   try sentiment classifier on single list
+    # pred_texts = ["ever since joes has changed hands it 's just gotten worse and worse .", "I'm so happy today, I want to cry happy tears. I love everything!"]
+    # print(classify_sentiment(pred_texts))
+
+    #   try sentiment classifier on dummy dataset
+    df = pd.read_csv("outputs/yelp_dummy-zero_shoot.csv", header=None)
+    generated = list(df[0])
+    print(score_sentiment(generated, output_file="results/yelp_dummy_score.csv"))
+    
