84c5b8cfbdbc3adcaa845c9a6d5a5aefe34a615f
David Peng <david.peng@yale.edu>
2023-04-26 12:46:14 -0400

    starting eval on GYAFC test for zero shot

--- chatgpt.py
+++ chatgpt.py
@@ -18,6 +18,8 @@ MAX_TOKENS = 20
 def evaluate_prompt(prompt):
     """
     Evaluate a single prompt by calling to OpenAI
+
+    Returns (output, total_tokens)
     """
     # Make API call to OpenAI
 
@@ -31,24 +33,39 @@ def evaluate_prompt(prompt):
     try:
         generated_output = completion.choices[0].message.content
         # print(f"\tGenerated: {generated_output}")
-        return generated_output
+        total_tokens = completion.usage.total_tokens
+
+        return generated_output, total_tokens
     except Exception as e:
         print(f"Failed with prompt \"{prompt}\". Exception: {e}")
 
 
-def eval_df(df, dataset_name, input_col, target_style_col, prompt_style="zero_shoot", output_dir="outputs"):
+def eval_df(df, input_col, target_style_col, dataset_name, prompt_style="zero_shoot", output_dir="outputs"):
     """Call rows of dataframe to OpenAI API
     
     input_col: column name of input sentences
     target_style_col: column name of target style
     """
-    
+
+    tmp_row = df.iloc[0]
+    input_sentence = tmp_row[input_col]
+    target_style = tmp_row[target_style_col]
+    tmp_prompt = gen_prompt(input_sentence, target_style, prompt_style=prompt_style)
+
+    print(f"Length of dataset: {len(df)}")    
+    continue_proc = ""
+    while continue_proc not in ["Y", "N"]:
+        continue_proc = input(f"Is this the prompt you expect?\n'{tmp_prompt}'\nEnter Y/N: ")
+    if continue_proc == "N":
+        print("Exiting...")
+        return
+    print("Continuing...")
+
     output_csv = f"{output_dir}/{dataset_name}-{prompt_style}.csv"
-    print(f"Writing to {output_csv}")
-
-
+    print(f"Writing output to {output_csv}")
 
     success = 0
+    total_tokens = 0
     generated_outputs = []
     for index, row in tqdm(df.iterrows(), total=len(df)):
         input_sentence = row[input_col]
@@ -57,14 +74,16 @@ def eval_df(df, dataset_name, input_col, target_style_col, prompt_style="zero_sh
         while success <= index:
             try:
                 prompt = gen_prompt(input_sentence, target_style, prompt_style=prompt_style)
-                generated_outputs.append(evaluate_prompt(prompt))
+                generated_output, tokens = evaluate_prompt(prompt)
+                generated_outputs.append(generated_output)
+                total_tokens += tokens
                 pd.DataFrame(generated_outputs).to_csv(output_csv, index=False, header=False)
                 success += 1
             except (openai.error.RateLimitError, openai.error.APIError) as e:
                 print(e._message)
                 set_trace()
             # time.sleep(20) # no longer needed, rate limit was increased...?
-
+    print(f"Total tokens used: {total_tokens}")
 
 if __name__ == "__main__":
     #   test single prompt
@@ -78,7 +97,14 @@ if __name__ == "__main__":
 
     #   test GYAFC dummy
     df = pd.read_csv("GYAFC/GYAFC_dummy.csv", keep_default_na=False)
-    eval_df(df, "GYAFC_dummy", "input", "target_style", prompt_style="zero_shoot")
+    eval_df(df.iloc[0:1, :], "input", "target_style", "GYAFC_dummy", prompt_style="zero_shoot")
 
     #   test GYAFC full (1332)
     # df = pd.read_csv("GYAFC/informal_to_formal.csv", keep_default_na=False)
+
+    #   test 100 of GYAFC
+    # df = pd.read_csv("GYAFC/GYAFC_test.csv", keep_default_na=False)
+    # df = df.iloc[0:100, :]
+    # print(len(df))
+    # eval_df(df, "input", "target_style", "GYAFC_100", prompt_style="zero_shoot")
+    
