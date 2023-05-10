c51d18148bb5a92ecf6c7b8cf378c74b78ddef07
David Peng <david.peng@yale.edu>
2023-05-10 06:44:26 -0400

    fix utils

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def gen_prompt(input_sentence, to_style ,prompt_style="zero_shoot"):
   elif prompt_style == "augment_shoot":
     answer = '''Here is some text: "When the doctor asked Linda to take the medicine, he smiled and gave her a lollipop". Here is a rewrite of the text, which is more scary: "When the doctor told Linda to take the medicine, there had been a malicious gleam in her eye that Linda didn't like at all" Here is some text: "They asked loudly, over the sound of the train". Here is a rewrite of the text, which is more intense: "They yelled aggressively, over the clanging of the train" Here is some text:  \"{}\". Here is a rewrite of the text, which is more {}:'''.format(input_sentence,to_style)
   elif prompt_style == "paraphrase":
-    answer = '''Here is some text: \"{}\". Here is a paraphrase of the text, which removes the style but preserves the content:'''
+    answer = '''Here is some text: \"{}\". Here is a paraphrase of the text, which removes the style but preserves the content:'''.format(input_sentence)
   else:
     answer = '''Rewrite to be more {}:\n{}'''.format(to_style, input_sentence)
   return answer
